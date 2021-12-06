104008c89820ccb13514f762c0f2d3a66ff3ef09
Seokjin Yoon <sjy5386@gmail.com>
2021-12-06 22:08:39 +0900

    Add FreeCell class

--- freecell.py
+++ freecell.py
@@ -22,3 +22,16 @@ class PlayingCard:
         d = {1: 'A', 11: 'J', 12: 'Q', 13: 'K'}
         number = d[self.number] if self.number in d.keys() else self.number
         return f'{self.suit.suit}{number}'
+
+
+class FreeCell:
+    def __init__(self):
+        self.cell = []
+
+    def push(self, element: PlayingCard):
+        if len(self.cell) < 4:
+            self.cell.append(element)
+
+    def pop(self, index: int):
+        if 0 <= index < len(self.cell):
+            return self.cell.pop(index)
